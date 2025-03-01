a1d249fd4314a721b93a373a60024ddaea1f44c6
WassimMensi <wassim.mensi@ecole-isitech.fr>
2025-03-01 13:55:42 +0100

    Add Login component with template, script, and styles; update README for component creation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <MangaList />
+    <!-- <Login /> -->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import MangaList from "./components/MangaList/MangaList.vue";
+import Login from "./components/Login/Login.vue";
 
 export default defineComponent({
   components: {
     MangaList,
+    Login,
   },
 });
 </script>
---
+++ frontend/src/components/Login/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="login">
+        <h1>Login</h1>
+        <form @submit.prevent="login">
+            <input type="text" v-model="username" placeholder="Username" required>
+            <input type="password" v-model="password" placeholder="Password" required>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+
+<script src="./Login.ts"></script>
+
+<style>
+    @import './Login.css';
+</style>
