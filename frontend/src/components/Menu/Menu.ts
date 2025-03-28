a2719af308f9d9e309514ce61adbb55274a5a6a8
DarmonNoah <noah.darmon@ecole-isitech.fr>
2025-03-28 11:22:06 +0100

    Init Menu component

---
+++ frontend/src/components/Menu/Menu.ts
@@ -0,0 +1,8 @@
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'NomComposant',
+    setup() {
+        // code de la page
+    }
+});
---
+++ frontend/src/components/Menu/Menu.vue
@@ -0,0 +1,9 @@
+<template>
+    <!-- contenu de la page html -->
+</template>
+
+<script src="./NomComposant.ts"></script>
+
+<style>
+    @import './NomComposant.css';
+</style>
