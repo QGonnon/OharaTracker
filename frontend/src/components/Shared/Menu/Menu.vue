8cc8f3622a06a05085697374fbba4b6f9341a606
QGonnon <150007582+QGonnon@users.noreply.github.com>
2026-09-18 19:17:22 +0200

    Add OharaTracker logo SVG and update Menu component to include logo

--- frontend/src/components/Shared/Menu/Menu.vue
+++ frontend/src/components/Shared/Menu/Menu.vue
@@ -4,6 +4,7 @@
 
       <!-- Logo -->
       <h1 class="text-2xl font-bold">
+        <img src="../../../assets/OharaTracker.svg" alt="Logo" class="inline-block w-8 h-8 mr-2" />
         <RouterLink to="/" class="hover:opacity-80 transition-opacity">Ohara Tracker</RouterLink>
       </h1>
 
